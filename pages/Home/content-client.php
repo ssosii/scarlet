c65e0ee93c95e162dfe10f62fa397d36033779bc
Lukasz <Lukasz.sosnecki@gmail.com>
2022-12-28 19:34:53 +0100

    clients

--- pages/Home/content-client.php
+++ pages/Home/content-client.php
@@ -1,3 +1,50 @@
-<section id="clients" style="height:50vh;background-color:red;display:flex;align-items:center;justify-content:center">
-    KLIENCI
+<section id="clients">
+
+    <div class="clients__container">
+        <h1 class="title">Nasi <em>Partnerzy</em></h1>
+        <!-- <p class="desc">Jesteśmy dumni, że możemy współpracować z niektórymi z najbardziej znanych kanadyjskich marek
+            oraz kilkoma,
+            które odciskają swoje piętno na nowych, ekscytujących sposobach. Pomyśl tylko, jak wspaniale wyglądałbyś w
+            rodzinie!</p> -->
+    </div>
+    <div class="clients__container">
+        <div class="single">
+            <object type="image/svg+xml" data="<?php bloginfo('template_url'); ?>/assets/img/prius.svg" class="image">
+
+            </object>
+        </div>
+
+        <div class="single">
+            <object type="image/svg+xml" data="<?php bloginfo('template_url'); ?>/assets/img/prius.svg" class="image">
+
+            </object>
+        </div>
+
+        <div class="single">
+            <object type="image/svg+xml" data="<?php bloginfo('template_url'); ?>/assets/img/prius.svg" class="image">
+
+            </object>
+        </div>
+
+        <div class="single">
+            <object type="image/svg+xml" data="<?php bloginfo('template_url'); ?>/assets/img/prius.svg" class="image">
+
+            </object>
+        </div>
+
+        <div class="single">
+            <object type="image/svg+xml" data="<?php bloginfo('template_url'); ?>/assets/img/prius.svg" class="image">
+
+            </object>
+        </div>
+
+        <div class="single">
+            <object type="image/svg+xml" data="<?php bloginfo('template_url'); ?>/assets/img/prius.svg" class="image">
+
+            </object>
+        </div>
+
+
+    </div>
+
 </section>
